9fff6548dcba5810956a5ddacf66e46da9daea84
kubilik <yusuf-kubi@hotmail.com>
2023-07-22 19:41:36 +0300

    Start Booster is done

--- Puck Rush/Assets/Scripts/PlayerController.cs
+++ Puck Rush/Assets/Scripts/PlayerController.cs
@@ -2,24 +2,55 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
-    public float deceleration;
+    [Header("Puck Settings")]
+    [SerializeField] private float puckNormalspeed = 5f;
+    [SerializeField] private float puckCurrentSpeed;
+    [SerializeField] private float deceleration = 1f;
+    [Header("Booster Settings")]
+    [SerializeField] private float puckBoostSpeed = 10f;
+    [SerializeField] private float boostTime = 1f;
+
+    private float boostTimer = 0f;
+    private bool boosting = false;
+
+    private void Start()
+    {
+        boosting = false;
+    }
 
     void Update()
     {
         // Check if the left mouse button is pressed.
         if (Input.GetMouseButtonDown(0))
         {
-            // Move the puck forward at its current speed.
-            this.transform.Translate(0, 0, speed);
+            boosting = true;
+            puckCurrentSpeed = puckBoostSpeed;
         }
+
+        // Move the puck forward at its current speed.
+        transform.Translate(0, 0, puckCurrentSpeed * Time.deltaTime);
+
         // Decelerate the puck over time.
-        speed -= deceleration * Time.deltaTime;
+        if (!boosting)
+        {
+            puckCurrentSpeed -= deceleration * Time.deltaTime;
+        }
 
         // If the puck's speed is below a certain threshold, stop it.
-        if (speed < 0.1f)
+        if (puckCurrentSpeed < 0.1f)
         {
-            speed = 0.0f;
+            puckCurrentSpeed = 0.0f;
+        }
+
+        if (boosting)
+        {
+            boostTimer += Time.deltaTime;
+            if (boostTimer > boostTime)
+            {
+                puckCurrentSpeed = puckNormalspeed;
+                boostTimer = 0f;
+                boosting = false;
+            }
         }
     }
 }
