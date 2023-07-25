6993089722d89dc6ed49a1136fd78e12d0cdd5d3
Arkeyuen <orkunarslanturk@stu.aydin.edu.tr>
2023-07-25 17:03:46 +0300

    patlama efekti deniyoz(pc patlayabilir)

--- Puck Rush/Assets/Orkun/Scripts/Durability.cs
+++ Puck Rush/Assets/Orkun/Scripts/Durability.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class Durability : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float cylinderSize = .2f;
+    public int cylindersInRow = 5;
+
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.tag == "HardWall")
+        {
+            explode();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void explode()
     {
-        
+        gameObject.SetActive(false);
+
+        for (int x = 0; x < cylindersInRow; x++)
+        {
+            for (int y = 0; x < cylindersInRow; y++)
+            {
+                for (int z = 0; x < cylindersInRow; z++)
+                {
+                    createPiece(x, y, z);
+                }
+            }
+        }
+    }
+
+    void createPiece(int x, int y, int z)
+    {
+        GameObject piece;
+        piece = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+
+        piece.transform.position = transform.position + new Vector3(cylinderSize*x, cylinderSize*y, cylinderSize * z);
+        piece.transform.localScale = new Vector3(cylinderSize, cylinderSize, cylinderSize);
+
+        piece.AddComponent<Rigidbody>();
+        piece.GetComponent<Rigidbody>().mass = cylinderSize;
     }
 }
