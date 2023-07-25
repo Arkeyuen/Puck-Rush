bd886702a5ae1271591afd2631b0115740db5d33
Arkeyuen <orkunarslanturk@stu.aydin.edu.tr>
2023-07-25 17:16:59 +0300

    patlamaya çalışıyoz

--- Puck Rush/Assets/Orkun/Scripts/Durability.cs
+++ Puck Rush/Assets/Orkun/Scripts/Durability.cs
@@ -7,6 +7,19 @@ public class Durability : MonoBehaviour
     public float cylinderSize = .2f;
     public int cylindersInRow = 5;
 
+    float cylindersPivotDistance;
+    Vector3 cylindersPivot;
+
+    public float explosionForce = 50f;
+    public float explosionRadius = 4f;
+    public float explosionUpward = 0.4f;
+
+    private void Start()
+    {
+        cylindersPivotDistance = cylinderSize * cylindersInRow / 2;
+
+        cylindersPivot = new Vector3(cylindersPivotDistance, cylindersPivotDistance, cylindersPivotDistance);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -30,6 +43,17 @@ public class Durability : MonoBehaviour
                 }
             }
         }
+
+        Vector3 explosionPos = transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, explosionRadius);
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(explosionForce, transform.position, explosionRadius, explosionUpward);
+            }
+        }
     }
 
     void createPiece(int x, int y, int z)
@@ -37,7 +61,7 @@ public class Durability : MonoBehaviour
         GameObject piece;
         piece = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
 
-        piece.transform.position = transform.position + new Vector3(cylinderSize*x, cylinderSize*y, cylinderSize * z);
+        piece.transform.position = transform.position + new Vector3(cylinderSize * x, cylinderSize * y, cylinderSize * z) - cylindersPivot;
         piece.transform.localScale = new Vector3(cylinderSize, cylinderSize, cylinderSize);
 
         piece.AddComponent<Rigidbody>();
